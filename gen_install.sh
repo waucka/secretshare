528c3f520c9d6aaf6da0b8e94c4b7df983a3c007
Alex Wauck <alexwauck@exosite.com>
2016-04-01 15:19:23 -0500

    Tweaked install script generator a bit

--- gen_install.sh
+++ gen_install.sh
@@ -28,14 +28,14 @@ echo 'I like to run random code from the Internet as root without reading it!  c
 URL="https://s3-$DEPLOY_BUCKET_REGION.amazonaws.com/$DEPLOY_BUCKET/client/$TARGET_OS-amd64/$LATEST_VERSION/secretshare"
 
 if test -f `which curl`; then
-    curl -o /usr/local/bin/secretshare "\$URL"
+    sudo curl -o /usr/local/bin/secretshare "\$URL"
 elif test -f `which wget`; then
-    wget -O /usr/local/bin/secretshare "\$URL"
+    sudo wget -O /usr/local/bin/secretshare "\$URL"
 else
     echo 'No curl or wget!  Install one of these first.'
     exit 1
 fi
 
-chmod +x /usr/local/bin/secretshare
+sudo chmod +x /usr/local/bin/secretshare
 
 EOF
