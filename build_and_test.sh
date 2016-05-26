5fb474326f9b9b5f91beebb1d01d0a96a003fc1c
Alex Wauck <alexwauck@exosite.com>
2016-05-26 09:32:57 -0500

    Updated build_and_test.sh to support platform-specific builds

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,12 +3,22 @@
 go get ...
 go get gopkg.in/check.v1
 
-if ! make; then
-    exit 1
-fi
+if [ -n "$1" ]; then
+    if ! make $1; then
+	exit 1
+    fi
 
-if ! make test; then
-    exit 1
+    if ! make test_$1; then
+	exit 1
+    fi
+else
+    if ! make; then
+	exit 1
+    fi
+
+    if ! make test; then
+	exit 1
+    fi
 fi
 
 exit 0
